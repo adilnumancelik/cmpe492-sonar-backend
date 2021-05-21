cda31b9433db0d0c5b4f480e798eb0556a285a12
adilnumancelik <adil@shopihq.com>
2021-05-21 22:07:24 +0300

    Add missing import

--- sonar_backend/settings.py
+++ sonar_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-heroku'
+    'django-heroku',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [

06708589f2704438bcf748ebfda99a9e1a67991e
adilnumancelik <adil@shopihq.com>
2021-05-21 22:15:40 +0300

    Add app

--- sonar_backend/settings.py
+++ sonar_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django-heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'django-heroku'
 ]
 
 MIDDLEWARE = [

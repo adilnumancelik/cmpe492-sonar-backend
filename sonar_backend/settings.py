7bc50bf02258e24620142e2ead1e348b08447c85
adilnumancelik <adil@shopihq.com>
2021-05-22 20:43:13 +0300

    Add swagger docs

--- sonar_backend/settings.py
+++ sonar_backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- sonar_backend/urls.py
+++ sonar_backend/urls.py
@@ -19,11 +19,30 @@ from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 from api.views import DummyViewSet
 from api import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Sonar API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@sonar.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 router = DefaultRouter()
 router.register(r'dummies', DummyViewSet)
 
 urlpatterns = [
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('hello/', views.main_view),
